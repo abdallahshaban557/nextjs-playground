824f1afe863f01977d19e32bc3bd33801625b4d0
Abdallah Shaban <abdallah.w.shaban@gmail.com>
2022-12-20 07:58:21 -0800

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "nextjsplayground": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "analytics": {
+        "nextjsplaygroundKinesis": {
+            "kinesisStreamArn": "string",
+            "kinesisStreamId": "string",
+            "kinesisStreamShardCount": "string"
+        }
+    }
+}
