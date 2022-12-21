ed747eec9451984f99ead5182a7822aea9e6abc6
Abdallah Shaban <abdallah.w.shaban@gmail.com>
2022-12-21 13:33:31 -0800

    No dependencies added to NextJS app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,30 @@
 // import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Amplify, Analytics, AWSKinesisProvider } from 'aws-amplify';
-import awsconfig from '../src/aws-exports';
+// import { Amplify, Analytics, AWSKinesisProvider } from 'aws-amplify';
+// import awsconfig from '../src/aws-exports';
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 
-Analytics.addPluggable(new AWSKinesisProvider());
+// Analytics.addPluggable(new AWSKinesisProvider());
 
-Analytics.configure({
-  AWSKinesis: {
-    // OPTIONAL -  Amazon Kinesis service region
-    region: 'us-west-2',
+// Analytics.configure({
+//   AWSKinesis: {
+//     // OPTIONAL -  Amazon Kinesis service region
+//     region: 'us-west-2',
 
-    // OPTIONAL - The buffer size for events in number of items.
-    bufferSize: 1000,
+//     // OPTIONAL - The buffer size for events in number of items.
+//     bufferSize: 1000,
 
-    // OPTIONAL - The number of events to be deleted from the buffer when flushed.
-    flushSize: 100,
+//     // OPTIONAL - The number of events to be deleted from the buffer when flushed.
+//     flushSize: 100,
 
-    // OPTIONAL - The interval in milliseconds to perform a buffer check and flush if necessary.
-    flushInterval: 5000, // 5s
+//     // OPTIONAL - The interval in milliseconds to perform a buffer check and flush if necessary.
+//     flushInterval: 5000, // 5s
 
-    // OPTIONAL - The limit for failed recording retries.
-    resendLimit: 5
-  }
-});
+//     // OPTIONAL - The limit for failed recording retries.
+//     resendLimit: 5
+//   }
+// });
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
